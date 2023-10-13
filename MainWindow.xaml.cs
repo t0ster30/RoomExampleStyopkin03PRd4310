a7bb37681c473ddb7dcd6c43830d3abe1baabd8e
t0ster30 <29415403@s.mfua.ru>
2023-10-13 13:05:12 +0300

    rooms.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using 
 
 namespace RoomExampleStyopkin03PRd4310
 {
