5f0327c70998cf6504bb12d30ac3ac428aea4b5b
t0ster30 <29415403@s.mfua.ru>
2023-10-13 15:04:06 +0300

    button 2 works.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using 
+using RoomLibraryStyopkin03PRd4310;
 
 namespace RoomExampleStyopkin03PRd4310
 {
@@ -21,9 +21,50 @@ namespace RoomExampleStyopkin03PRd4310
     /// </summary>
     public partial class MainWindow : Window
     {
+        Room room1 = new Room();
+        Room room2 = new Room();
+        Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonOpen1_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpen2.IsEnabled = true;
+            room1.RoomLength = rnd.Next(2, 11);
+            room1.RoomWidth = rnd.Next(2, 11);
+            int numP = rnd.Next(1, 9);
+
+            LabelLength1.Content = room1.RoomLength;
+            LabelWidth1.Content = room1.RoomWidth;
+            LabelNumPerson1.Content = numP;
+
+            LabelPerimeter1.Content = room1.RoomPerimetr();
+            LabelArea1.Content = room1.RoomArea();
+            LabelPersonArea1.Content = Math.Round(room1.PersonArea(numP),3);
+        }
+
+        private void ButtonOpen2_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonAll.IsEnabled = true;
+            room2.RoomLength = Convert.ToDouble(TBLength2.Text);
+            room2.RoomWidth = Convert.ToDouble(TBWidth2.Text);
+            int numP = Convert.ToInt32(TBNumPerson2.Text);
+
+            LabelPerimeter2.Content = room2.RoomPerimetr();
+            LabelArea2.Content = room2.RoomArea();
+            LabelPersonArea2.Content = Math.Round(room2.PersonArea(numP), 3);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonAll_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
